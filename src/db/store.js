464a6c332c24dbab23ee1d3d9b6fcdaa86d9e441
Azu <azucenaz_lu@lagash.com>
2018-10-22 20:45:04 -0300

    changed store so it maintains the state when the page is refreshed

--- src/db/store.js
+++ src/db/store.js
@@ -7,12 +7,37 @@ export const history = createBrowserHistory()
 
 const enhancers = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)
 
+const saveState = state => {
+  try {
+    const serialisedState = JSON.stringify(state)
+    window.localStorage.setItem('state', serialisedState)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const loadState = () => {
+  try {
+    const serialisedState = window.localStorage.getItem('state')
+    if (!serialisedState) return undefined
+    return JSON.parse(serialisedState)
+  } catch (err) {
+    return undefined
+  }
+}
+const oldState = loadState()
+
 const store = createStore(
   rootReducer,
+  oldState,
   compose(
     applyMiddleware(thunk),
     enhancers
   )
 )
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 export default store
